00f34d4a39e282168b78812b22d75e3ae85be27c
Jannik Feuerhahn <jannik@feuer.dev>
2020-09-11 14:39:09 +0800

    better variable naming and longer time between cycles

--- scramble.ts
+++ scramble.ts
@@ -3,17 +3,22 @@ class Scrambler {
   /**
    * Timeout between each random character
    */
-  private timeout = 5;
+  private toChar = 5;
+
+  /**
+   * Timeout between each sentence
+   */
+  private toSentence = 2000;
 
   /**
    * Random characters to pick from
    */
-  private characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'x', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'X', '#', '%', '&', '-', '+', '_', '?', '/', '\\', '='];
+  private chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'x', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'X', '#', '%', '&', '-', '+', '_', '?', '/', '\\', '='];
 
   /**
    * Texts to shuffle through
    */
-  private texts = ["Hey, I'm Jannik", "Hi, ich bin Jannik", "嗨，我是季焱"];
+  private sentences = ["Hey, I'm Jannik", "Hi, ich bin Jannik", "嗨，我是季焱"];
 
   /**
    * The element to be used
@@ -23,12 +28,12 @@ class Scrambler {
   /**
    * currently scrambled text
    */
-  private index = 0;
+  private idxSentence = 0;
 
   /**
    * currently scrambled letter
    */
-  private offset = 0;
+  private idxChar = 0;
 
   /**
    * Times to show a random Char 
@@ -38,17 +43,20 @@ class Scrambler {
   private partialString = "";
 
   public scramble() {
-    const currentString = this.texts[this.index];
-    this.partialString = currentString.substring(0, this.offset);
+    const currentString = this.sentences[this.idxSentence];
+    this.partialString = currentString.substring(0, this.idxChar);
 
     this.doRandomiserEffect(() => {
-      if (this.offset <= currentString.length) {
+      if (this.idxChar <= currentString.length) {
         this.scramble();
-        this.offset++;
+        this.idxChar++;
       } else {
-        this.offset = 0;
-        this.index = (this.index+1) % (this.texts.length);
-        this.scramble();
+        this.idxChar = 0;
+        this.idxSentence = (this.idxSentence+1) % (this.sentences.length);
+        setTimeout(() => {
+          this.scramble();
+        }, this.toSentence);
+        
       }
     });
   }
@@ -61,7 +69,7 @@ class Scrambler {
           this.element.textContent = this.partialString;
         } else {
           // Replaces the last character of partialString with a random character
-          this.element.textContent = this.partialString.substring(0, this.partialString.length - 1) + randomCharacter(this.characters);
+          this.element.textContent = this.partialString.substring(0, this.partialString.length - 1) + randomCharacter(this.chars);
         }
 
         this.iterations--;
@@ -70,7 +78,7 @@ class Scrambler {
         this.iterations = 10;
         callback();
       }
-    }, this.timeout);
+    }, this.toChar);
   }
 }
 
